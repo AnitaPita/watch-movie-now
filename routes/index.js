c0c7fa398d54d2407c7eb1a10c6e448578b419d1
Brandon Danis <brandon@spottedu.co>
2016-04-07 18:27:03 -0400

    Fixing merge conflict

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ var pg = require('pg');
 var router = express.Router();
 var http = require('http');
 var vd = require('../videodata.json');
-var md = require( '../imdb.json');
+//var md = require( '../imdb.json');
 var request = require('request');
 var url = 'https://yts.ag/api/v2/list_movies.json?genre=horror&page=2';
-var connectionString = "pg://apope007:LUNCANI1!@web0.site.uottawa.ca:15432/apope007";
+var pg = require('pg'); //Added by A.P for router.post
+var connectionString = "pg://apope007:LUNCANI1!@web0.site.uottawa.ca:15432/apope007"; //Added by A.P for router.post
+
+
 
 request({
     url: url,
@@ -17,29 +20,31 @@ request({
     if (!error && response.statusCode === 200) {
         //var banana = body;
 
-        router.get('/', function (req, res, next) {
-           var lol = function(){ pg.connect(connectionString, function(err, client, done) {
-                // Handle connection errors
+        router.get('/', function (req, res) {
+
+            var results = [];
+            var nice;
+
+            var results = [];
+            var nice;
+
+            pg.connect(connectionString, function(err, client, done) {
+         	// Handle connection errors
                 if(err) {
-                    done();
-                    console.log(err);
-                    return res.status(500).json({ success: false, data: err});
-                }
-                else{
-                    console.log("I'm alliiiiive")
-                }
-                var query = client.query("SELECT * FROM movie");
-                query.on('row', function(row) {
-                    console.log(row);
-
-                });
-
-                query.on('end', function() { client.end();});
-            });}
-            res.render('index', {title: 'You Watch Movie Now!', taco: vd, imdb : md, queryq: lol()});
-
+           	        done();
+           	        console.log(err);
+           	        return res.status(500).json({ success: false, data: err});
+                }else{
+         	        console.log("I'm alliiiiive")
+         		}
+         		var query = client.query("SELECT * FROM movie", function(err, answer){
+         			if(!err){
+         				res.render('index', {title: 'not Youssef', taco: vd, imdb : answer['rows'] });//results = answer['rows'];
+         				//res.json(answer['rows']);
+         			}
+         		});
+         	});
         });
-
     }
 });
 
